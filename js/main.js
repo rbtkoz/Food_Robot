7b1c07ae54f21016b1d887fdbaedad02be243909
AK <kozovski.a@gmail.com>
2014-02-23 23:22:07 -0500

    Yummly API functionality

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@ $(document).ready(function(){
           console.log("imthere");
         }
       }
+      // $(".list-title").css("display","block");
     }  
     });
   });
@@ -71,11 +72,65 @@ $(document).ready(function(){
   $(".btn").click(function(){
     // alert("hello");
     // $( "button ,.btn,.btn-default, #scrape" ).removeClass();
+    $(".form-group, .and").css("display","none");
     $("#scrape").remove();
+    $(".and").css("margin-left","300px");
+     $(".form-control").css("margin-left","132px");
+     $(".recipe").css("margin-top","0px");
+  
 
     
   })
 
-  
+//   $('#recipe-search').click(function(event) {
+// event.preventDefault();
+// var recipestr = $('#recipe-value').val();
+// var url = "http://www.yummly.com/api/recipesq="+keywords+"&_app_id=<snipped-app-id>&_app_key=<snipped-api-key>&";
+// console.log(recipestr);
+// $.ajax({
+//   type: 'POST',
+//   url: "http://127.0.0.1:8585",
+//   data: recipestr,
+//   success: function(msg) {
+//     alert("success");  },
+//   error: function(XMLHttpRequest, textStatus) {
+    
+//   }
+// })
+// })
+$('#recipe-search').click(function(event) {
+event.preventDefault();
+var keywords = $('#recipe-value').val();
+console.log(keywords);
+var url ="http://api.yummly.com/v1/api/recipes?_app_id=d77dde75&_app_key=4a28aa62c4333836b11ef718e632218f&"+keywords+"&requirePictures=true"+"&callback=data";  
+    $.ajax({                                                                            
+      type: 'GET',
+       url: url,
+       dataType: 'jsonp',
+       //dataType: 'jsonp json'
+       success: function(data) { 
+        var data1 = data.matches;
+        console.log(data1);
+         for (var i=0; i<data1.length;i++){
+          
+        if(i%2 ===0){
+          $('ul.list-recipe').append("<li class ='1'><h4>"+data1[i].id+"</h4></li>");
+          $("li.1").css("background","#f6f6f6 ");
+          console.log("imhere");
+          // console.log(i);
+        }else{
+          $('ul.list-recipe').append("<li class ='2'><h4>"+data1[i].id+"</h4></li>");
+          $("li.2").css("background","#f1f1f1");
+          console.log("imthere");
+        }
+      }
+        },
+
+       error: function(data, data2) { console.log(data); },
+       //jsonp: false,
+       //jsonpCallback: 'recipeGet'                                          
+  });
+});
+
 });
 
